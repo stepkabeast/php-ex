02537023bd42c99b4f815f92425168759fb02d2b
Stepan <stepak038@gmail.com>
2024-07-07 14:12:13 +0800

    add last ex_files

---
+++ php-kovalevsa/ex_24_25.php
@@ -0,0 +1,14 @@
+<?php
+/* Выведи в браузере любой код на PHP. Например:
+if($variable){
+  echo "true";
+} else {
+  echo "false";
+}
+ */
+$variable = 'test';
+if(isset($variable)){// проверяем существование переменной
+  echo "true";
+} else {
+  echo "false";
+}
---
+++ php-kovalevsa/ex_27.php
@@ -0,0 +1,9 @@
+<?php
+// Как проверить, равно значение переменной null? Напиши пример. 
+$variable = null;
+
+if (is_null($variable)) {
+    echo "Переменная равна null";
+} else {
+    echo "Переменная не равна null";
+}
